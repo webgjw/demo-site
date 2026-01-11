fb0ab2a8774b0beb23d9aa2954501cfabde96368
webgjw <1096026808@qq.com>
2026-01-11 21:17:42 +0800

    update main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
 import { createApp } from 'vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
@@ -9,4 +11,4 @@ const app = createApp(App)
 for (const name in ElIcons) {
     app.component(name, ElIcons[name])
 }
-app.use(router).use(store).use(i18n).mount('#app')
+app.use(ElementPlus).use(router).use(store).use(i18n).mount('#app')
